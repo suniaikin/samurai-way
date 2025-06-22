0faa122c72b7d9183286e367676ac39018a3ded6
suniaikin <class.suniaikin@gmail.com>
2025-06-22 12:37:11 -0700

    new variables

--- zero/script.js
+++ zero/script.js
@@ -1,5 +1,6 @@
-alert("привет, затупок!");
-alert("hi, dump!");
-alert("привет, идиот!");
-alert("hi, idiot!");
-
+let number1 = 100
+let number2 = 500
+let sum = number1 + number2
+console.log("The sum of " + number1 + " and " + number2 + " is " + sum)
+let difference = number2 - number1
+console.log("The difference between " + number2 + " and " + number1 + " is " + difference)

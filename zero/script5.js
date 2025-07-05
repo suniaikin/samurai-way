ab9a6a01e8cd8f0058f9945558674c130c3fa318
suniaikin <class.suniaikin@gmail.com>
2025-07-04 17:47:30 -0700

    arrows3 learning

---
+++ zero/script5.js
@@ -0,0 +1,31 @@
+// Массив оценок студента
+/* let grade1 = 5
+let grade2 = 4
+let grade3 = 3
+let grade4 = 5
+let grade5 = 4
+
+console.log(grade1)
+console.log(grade3)
+
+// let grades = [5, 4, 3, 5, 4]
+//
+// let extractedGrade = grades[0]
+console.log(grades)
+console.log(grades[0])
+
+console.log(extractedGrade)*/
+
+// Массив названий книг в библиотеке
+
+let book1 = "Война и мир"
+let book2 = "Мастер и Маргарита"
+let book3 = "Преступление и наказание"
+
+let books = ["Война и мир", "Мастер и Маргарита", "Преступление и наказание"]
+
+console.log(books[1])
+
+const masterAndMargo = books[1] // "Мастер и Маргарита"
+
+console.log(masterAndMargo)

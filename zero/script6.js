9226650a316f93c21f11318ae3a4ade038657126
suniaikin <class.suniaikin@gmail.com>
2025-07-04 17:04:07 -0700

    arrows2 learning

---
+++ zero/script6.js
@@ -0,0 +1,12 @@
+// "Любите ли вы Брамса?"
+
+let answer1 = true
+let answer2 = false
+let answer3 = true
+let answer4 = true
+
+let answers = [true, false, true, true ]
+
+console.log(answers[1])
+let answer = answers[1]
+console.log(answer)

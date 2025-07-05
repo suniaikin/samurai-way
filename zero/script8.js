b61796093ffbb04dd046f9841dd26890a2824a76
suniaikin <class.suniaikin@gmail.com>
2025-07-04 18:11:13 -0700

    arrows3 learning

---
+++ zero/script8.js
@@ -0,0 +1,28 @@
+// Массив списков покупок
+/*
+
+let shoppingList1Item1 = "молоко"
+let shoppingList1Item2 = "хлеб"
+let shoppingList1Item3 = "яйца"
+let shoppingList2Item1 = "сок"
+let shoppingList2Item2 = "мороженое"
+let shoppingList3Item1 = "корм для кота"
+let shoppingList3Item2 = "шампунь"
+*/
+
+let shoppingLists = [
+	["молоко", "хлеб", "яйца"],
+	["сок", "мороженое"],
+	["корм для кота", "шампунь"]
+]
+
+/*let shop = shoppingLists[1]
+console.log(shop)*/
+
+/*let shop2 = shoppingLists[2][1]
+console.log(shop2)*/
+
+let list = shoppingLists[2]
+let product = list[0]
+
+console.log(product)
---
+++ zero/script9.js
@@ -0,0 +1,5 @@
+const arr = [1,2,3,5,6,{},"",[]]
+const arrayLength = arr.length
+console.log(arrayLength)
+console.log(arrayLength*8)
+

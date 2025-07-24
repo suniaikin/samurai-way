b04bff4b2e42fae5a7433892ede57bafddfc8098
suniaikin <class.suniaikin@gmail.com>
2025-07-23 19:35:15 -0700

    added 'groceries lists'

--- zero/script7.2-groceries.js
+++ zero/script7.2-groceries.js
@@ -1,11 +1,16 @@
 let shopList = [
-	['milk','bread', 'eggs'],
+	['milk', 'bread', 'eggs'],
 	['juice', 'ice cream'],
 	['pet food', 'shampoo']
 ]
 
 for (let i = 0; i < shopList.length; i++) {
-	for (let j = 0; j < shopList[i].length; j++) {
-		console.log(shopList[i][j]);
+	let category = shopList[i];
+	document.write('List #', i+1, '<br/>')
+	document.write('-------', '<br/>')
+	for (let j = 0; j < category.length; j++) {
+		let item = category[j];
+		document.write(j + 1, "-", item, '<br/>');
 	}
+	document.write('<br/>')
 }

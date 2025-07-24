e352b880bc3e952acd60b0d275483b8aa360a251
suniaikin <class.suniaikin@gmail.com>
2025-07-23 18:52:11 -0700

    added 'groceries'

---
+++ zero/script7.1-people.js
@@ -0,0 +1,16 @@
+let people = [
+	{
+		name: "Anna",
+		lastName: "Smith",
+		birthYear: 1990
+	},
+	{
+		name: "Peter",
+		lastName: "Morgan",
+		birthYear: 1985
+	}
+]
+
+for (let i = 0; i < people.length; i++) {
+	console.log(people[i].lastName)
+}
---
+++ zero/shoppingLists.js
@@ -0,0 +1,13 @@
+// Массив списков покупок
+
+let shoppingLists = [
+	["молоко", "хлеб", "яйца"],
+	["сок", "мороженое"],
+	["корм для кота", "шампунь"]
+]
+
+for (let i = 0; i < shoppingLists.length; i++) {
+	for (let j = 0; j < shoppingLists[i].length; j++) {
+		console.log(shoppingLists[i][j])
+	}
+}
